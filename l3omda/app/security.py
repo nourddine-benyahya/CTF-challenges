d304d069bc4b1eda115d078497bee6057ad7e0a1
Nourddine benyahya <95361652+nourddine-benyahya@users.noreply.github.com>
2025-05-12 15:08:34 +0100

    Delete l3omda directory

--- l3omda/app/security.py
+++
@@ -1,7 +0,0 @@
-from app.models import User, Role
-from flask_security import Security, SQLAlchemyUserDatastore
-
-def init_security(app):
-    user_datastore = SQLAlchemyUserDatastore(db, User, Role)
-    security = Security(app, user_datastore)
-    return security
--- l3omda/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-until nc -z db 5432; do
-  echo "Waiting for PostgreSQL..."
-  sleep 1
-done
-
-python -m app.create_roles
-
-exec "$@"
